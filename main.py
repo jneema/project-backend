e30b4455b415b65e4042392957ccf8670440f30f
jneema <joeyshem33@gmail.com>
2023-11-10 19:15:30 +0300

    updates

--- main.py
+++ main.py
@@ -1,155 +1,41 @@
-import json
-import pandas as pd
-import re
-import emoji
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from textblob import TextBlob
-from sqlalchemy import create_engine, MetaData, Table, Column, Float, String, inspect
-from sqlalchemy.exc import IntegrityError
-from google_play_scraper import app, Sort, reviews
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from scraper import main
+import uuid  # For generating a unique table suffix
 
-# PostgreSQL database configuration
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/api"  
+app = FastAPI()
 
-# Function to clean text
-def clean_text(text):
-    if text is None:
-        return ""
-    # Lowercase
-    text = text.lower()
-    # Remove special characters and numbers
-    text = re.sub(r'[^a-z\s]', '', text)
-    return text
+origins = ["*"]
 
-# Function to extract emojis
-def extract_emojis(text):
-    emojis = re.findall(r'[^\w\s,]', text)
-    return ''.join(emojis)
-
-# Function to perform sentiment analysis
-def sentiment_analysis(text):
-    analysis = TextBlob(text)
-    return analysis.sentiment.polarity
-
-# Function to calculate positive review percentage
-def calculate_positive_percentage(df):
-    positive_reviews = df[df['sentiment_score'] > 0]
-    percentage_positive = (len(positive_reviews) / len(df)) * 100
-    return percentage_positive
-
-# # Scrape reviews for multiple apps with aliases
-# app_data = [
-#     {'alias': 'Betika', 'package_name': 'pl.loyaltyclub.betika'},
-#     {'alias': 'Betting App Kenya', 'package_name': 'ke.co.ipandasoft.premiumtipsfree'},
-#     {'alias': 'Odi Bets', 'package_name': 'com.odibet.app'},
-#     {'alias': 'Sport Pesa', 'package_name': 'sportpesa.app3'}
-# ]
-
-# # Scrape reviews for multiple apps with aliases
-# app_data = [
-#     {'alias': 'M-Pesa app', 'package_name': 'com.safaricom.mpesa.lifestyle'},
-#     {'alias': 'Safaricom app', 'package_name': 'com.safaricom.mysafaricom'},
-#     {'alias': 'Airtel App', 'package_name': 'com.airtel.africa.selfcare'},
-#     {'alias': 'Telkom', 'package_name': 'com.mytelkom'},
-#     {'alias': 'T-kash', 'package_name': 'com.tkashapp'}
-# ]
-
-# # Scrape reviews for multiple apps with aliases
-# app_data = [
-#     {'alias': 'Tala', 'package_name': 'com.inventureaccess.safarirahisi'},
-#     {'alias': 'Zenka', 'package_name': 'com.zenkafinance.microloans'},
-#     {'alias': 'Branch', 'package_name': 'com.branch_international.branch.branch_demo_android'},
-#     {'alias': 'Okash', 'package_name': 'com.loan.cash.credit.okash.nigeria'},
-# ]
-
-# Scrape reviews for multiple apps with aliases
-app_data = [
-    {'alias': 'Standard Chartered', 'package_name': 'com.scb.breezebanking.ke'},
-    {'alias': 'Equity', 'package_name': 'ke.co.equitygroup.equitymobile'},
-    {'alias': 'Absa', 'package_name': 'com.absa.ke.mobile.android.ui'},
-    {'alias': 'KCB', 'package_name': 'com.kcbbankgroup.android'},
-    {'alias': 'Vooma', 'package_name': 'com.kcbbankgroup.vooma.android'},
-    {'alias': 'Stanbic', 'package_name': 'com.cfcstanbicbank.app'},
-    {'alias': 'NCBA', 'package_name': 'com.nicbank.android'},
-    {'alias': 'I&M', 'package_name': 'com.imbank.digital.retail.app'},
-    {'alias': 'Consolidated', 'package_name': 'com.bank.myconso'},
-    {'alias': 'MCO-OPCASH', 'package_name': 'com.mcoopcash.retail'},
-    {'alias': 'PesaPap,Family Bank', 'package_name': 'com.mode.familykenya.ui'},
-    {'alias': 'Kenya Baroda Mobi', 'package_name': 'com.barodakenya'},
-    {'alias': 'Access Bank', 'package_name': 'com.craftsilicon.transnationalbank'},
-
-]
-
-# Create a SQLAlchemy engine and metadata
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-
-# Define the table structure for new and combined data
-table_structure_new = Table(
-    'banking_apps',
-    metadata,
-    Column('Alias', String, primary_key=True),
-    Column('Positive_Review_Percentage', Float)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-table_structure_combined = Table(
-    'all_reviews',
-    metadata,
-    Column('Alias', String, primary_key=True),
-    Column('Positive_Review_Percentage', Float)
-)
+class SearchQuery(BaseModel):
+    query: str
+    numReviews: int
 
-# Check if the tables already exist in the database
-inspector = inspect(engine)
+class ScrapeResponse(BaseModel):
+    message: str
+    positive_review_percentage: float
 
-# Create tables if they don't exist
-if not inspector.has_table('banking_apps'):
-    metadata.create_all(engine)
-    print("banking_apps table created")
+@app.post("/scrape_reviews/")
+async def scrape_reviews(search_query: SearchQuery):
+    query = search_query.query
+    num_reviews = search_query.numReviews
 
-if not inspector.has_table('all_reviews'):
-    metadata.create_all(engine)
+    # Generate a unique table name suffix for this request
+    table_suffix = str(uuid.uuid4())
 
-# Create a connection to the database
-with engine.connect() as conn:
-    for app_info in app_data:
-        package_name = app_info['package_name']
-        result, _ = reviews(
-            package_name,
-            lang='en',
-            country='us',
-            sort=Sort.NEWEST,
-            count=5000
-        )
-        
-        df = pd.DataFrame(result)
-        df['cleaned_content'] = df['content'].apply(clean_text)
-        df['extracted_emojis'] = df['cleaned_content'].apply(extract_emojis)
-        df['sentiment_score'] = df['cleaned_content'].apply(sentiment_analysis)
-        
-        # Calculate and store the positive review percentage along with the alias
-        percentage_positive = calculate_positive_percentage(df)
-        alias = app_info['alias']
-        
-        # Insert data into the new table
-        try:
-            conn.execute(table_structure_new.insert().values(Alias=alias, Positive_Review_Percentage=percentage_positive))
-            print(f"Inserted data for {alias} into new table")
-        except IntegrityError as e:
-            # Handle duplicate entry errors, if any
-            print(f"Error inserting data for {alias} into new table: {e}")
-            
-        # Insert data into the combined table
-        try:
-            conn.execute(table_structure_combined.insert().values(Alias=alias, Positive_Review_Percentage=percentage_positive))
-            print(f"Inserted data for {alias} into combined table")
-        except IntegrityError as e:
-            # Handle duplicate entry errors, if any
-            print(f"Error inserting data for {alias} into combined table: {e}")
+    # Call the scraping function with the table suffix
+    success = main(query, num_reviews, table_suffix)
 
-        conn.commit()
-# Close the database connection
-engine.dispose()
+    if success:
+        return ScrapeResponse(message="Scraping completed successfully", positive_review_percentage=0.0)
+    else:
+        return ScrapeResponse(message="Scraping failed", positive_review_percentage=0.0)
---
+++ scraper.py
@@ -0,0 +1,180 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from google_play_scraper import app, reviews, Sort
+from transformers import pipeline
+import nltk
+import re
+from tqdm import tqdm
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+import uuid  # For generating unique table names
+
+# Initializing NLTK
+nltk.download('punkt')
+
+# Function to perform sentiment analysis using DistilBERT
+def analyze_sentiment(text):
+    # Specifying the Hugging Face model name and revision
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+    revision = "af0f99b"
+
+    # Load the sentiment analysis pipeline with the specified model and revision
+    classifier = pipeline("sentiment-analysis", model=model_name, revision=revision)
+
+    results = classifier(text)
+    label = results[0]['label']
+    score = results[0]['score']
+    return label, score
+
+# Function to extract app ID from the app's URL
+def extract_app_id(url):
+    return url.split('=')[-1]
+
+# Function to clean text
+def clean_text(text):
+    if text is None:
+        return ""
+    # Lowercase
+    text = text.lower()
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-z\s]', '', text)
+    return text
+
+# Function to extract emojis
+def extract_emojis(text):
+    emojis = re.findall(r'[^\w\s,]', text)
+    return ''.join(emojis)
+
+# Function to create a new table to store reviews
+def create_reviews_table(engine, table_suffix):
+    Base = declarative_base()
+
+    # Define a model for the reviews table
+    class Review(Base):
+        __tablename__ = f'reviews_{table_suffix}'  # Use a unique table name
+        id = Column(Integer, Sequence('review_id_seq'), primary_key=True)
+        review = Column(String)
+        sentiment_label = Column(String)
+        sentiment_score = Column(Float)
+        created_at = Column(DateTime, default=datetime.datetime.now)
+
+    # Create the reviews table if it doesn't exist
+    Base.metadata.create_all(engine)
+
+    return Review
+
+# Function to scrape and save reviews to the database
+def scrape_and_save_reviews(app_id, num_reviews, session, Review):
+    review_result, _ = reviews(
+        app_id,
+        lang='en',
+        country='us',
+        sort=Sort.MOST_RELEVANT,
+        count=num_reviews,
+    )
+    progress_bar = tqdm(total=num_reviews, desc="Scraping Reviews", position=0)
+
+    unique_reviews = []
+    positive_reviews = 0
+    total_reviews = 0
+
+    for review_count, review in enumerate(review_result, start=1):
+        review_text = review['content']
+
+        # Clean the review text
+        cleaned_text = clean_text(review_text)
+        if not cleaned_text:
+            continue  # Skip empty reviews
+
+        if cleaned_text not in unique_reviews:
+            sentiment_label, sentiment_score = analyze_sentiment(cleaned_text)
+            if sentiment_label == "POSITIVE":
+                positive_reviews += 1
+            total_reviews += 1
+
+            # Create a Review record and add it to the database
+            new_review = Review(review=cleaned_text, sentiment_label=sentiment_label, sentiment_score=sentiment_score)
+            session.add(new_review)
+            session.commit()
+
+            unique_reviews.append(cleaned_text)
+
+        # Update the progress bar
+        progress_bar.update(1)
+
+    progress_bar.close()
+
+    positive_percentage = (positive_reviews / total_reviews) * 100
+    print(f"Positive Review Percentage: {positive_percentage:.2f}%")
+
+# Main function to control the entire process
+def main(search_query, num_reviews=100, table_suffix=None):
+    # Create a Chrome webdriver
+    driver = webdriver.Chrome()
+    driver.get("https://play.google.com/store")
+
+    # Find the search icon by class and aria-hidden attribute
+    search_icon = driver.find_element(By.CSS_SELECTOR, '.google-material-icons[aria-hidden="true"]')
+    search_icon.click()
+
+    # Wait for the search input to become visible
+    search_input = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Search Google Play"]')
+    while not search_input.is_displayed():
+        time.sleep(1)
+
+    # Enter the user-provided search query
+    search_input.send_keys(search_query + Keys.RETURN)
+    time.sleep(5)
+
+    # Find the link in the specified div
+    link = driver.find_element(By.CSS_SELECTOR, '.Qfxief')
+
+    # Get the app URL
+    app_url = link.get_attribute("href")
+    app_id = extract_app_id(app_url)
+    link.click()
+    time.sleep(5)
+    see_more_button = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="See more information on Ratings and reviews"]')
+    see_more_button.click()
+
+    # Scroll down to load reviews
+    SCROLL_PAUSE_TIME = 2
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Define your PostgreSQL database connection URL
+    db_url = "postgresql://postgres:12345@localhost/reviews"
+
+    # Generate a unique table name (e.g., based on a UUID)
+    table_suffix = str(uuid.uuid4())
+
+    # Create a SQLAlchemy engine and session
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    # Create a new table to store reviews
+    Review = create_reviews_table(engine, table_suffix)
+
+    # Create a session for database operations
+    session = Session()
+
+    # Scrape and save reviews to the database
+    scrape_and_save_reviews(app_id, num_reviews, session, Review)
+
+    # Close the database session
+    session.close()
+
+    driver.quit()
+
+if __name__ == '__main__':
+    main("your_search_query", num_reviews=100)
